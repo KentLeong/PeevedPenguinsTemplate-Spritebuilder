62df923036606e8b156337b4ab75c31dfc083980
KentLeong <EricKentLeongChu@gmail.com>
2015-02-15 00:42:03 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.h
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -1,13 +1,13 @@
 //
-//  Seal.m
+//  Gameplay.h
 //  PeevedPenguins
 //
 //  Created by Eric Chu on 2/15/15.
 //  Copyright (c) 2015 Apportable. All rights reserved.
 //
 
-#import "Seal.h"
+#import "CCNode.h"
 
-@implementation Seal
+@interface Gameplay : CCNode
 
 @end
